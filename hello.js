e763c42169ac3823273993c45fcbc6ea504c6cdd
Nick Landry <nick@mobility42.com>
2017-03-28 21:04:07 -0400

    Switched to waterfall conversation

--- hello.js
+++ hello.js
@@ -11,7 +11,24 @@ var bot = new builder.UniversalBot(connector);
 
 // Add in the dialog:
 // Dialogs have names that act like a path, this is our 'root' dialog.
-bot.dialog('/', function(session) {
-    // Send a message to the user via the user session.
-    session.send('Hello, my name is NodeChatBot')
-});
+// bot.dialog('/', function(session) {
+
+    // Send a message to the user via the user session:
+
+    // Fixed message
+    // session.send('Hello, my name is NodeChatBot')
+
+    // Echo message
+    // var userMessage = session.message.text;
+    // session.send('You said ' + userMessage);
+//});
+
+// Waterfall conversation
+bot.dialog('/', [
+    function(session) {
+        builder.Prompts.text(session, 'Please enter your name');
+    },
+    function(session, result) {
+        session.send('Hello, ' + result.response + '!')
+    }
+]);
